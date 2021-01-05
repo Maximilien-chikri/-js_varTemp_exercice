f80a6ed99cc2738ca363d15b9c59d331da4fdaa1
Maximilien <maximilienchikri@gmail.com>
2021-01-05 11:05:00 +0100

    ex 1 et 4

---
+++ public/js/main.js
@@ -0,0 +1,39 @@
+// 1
+let a = 1 ;
+let b = 2 ;
+let c = 3 ;
+let d = 4 ;
+let e = 5 ;
+let f = 6 ;
+let temp;
+
+// ex4
+
+temp = c
+
+c = a -> 1
+a = d -> 4
+d = b -> 2
+b = e -> 5
+e = f -> 6
+f = temp -> 3
+
+
+
+
+
+// variable
+
+// let temp;
+// temp = d
+// d = a
+// a = temp
+
+// temp = c
+// c = b
+// b = temp
+
+// console.log(a , b, c , d);
+
+
+
